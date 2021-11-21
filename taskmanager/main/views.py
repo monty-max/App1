89d5a2d9f792508f0dfdae75de10b7fbee241eb1
monty-max <vodopshin74@gmail.com>
2021-11-21 18:16:12 +0300

    1

--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
 from .forms import TaskForm
 
@@ -12,8 +12,20 @@ def about(request):
 
 
 def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма была неверной'
+
+
+
     form = TaskForm()
     context = {
-        'form': form
+        'form': form,
+        'error': error
     }
     return render(request, 'main/create.html', context)
