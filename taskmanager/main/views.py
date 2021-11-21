05f017dc1c28e5f430866146005bb87ae832205f
monty-max <vodopshin74@gmail.com>
2021-11-21 17:25:27 +0300

    1

---
+++ taskmanager/main/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from .models import Task
+from .forms import TaskForm
+
+def index(request):
+    tasks = Task.objects.order_by('-id')
+    return render(request, 'main/index.html', {'title': 'Главная страница сайта', 'tasks': tasks})
+
+
+def about(request):
+    return render(request, 'main/about.html')
+
+
+def create(request):
+    form = TaskForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'main/create.html', context)
